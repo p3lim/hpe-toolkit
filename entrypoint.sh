5951ed66a0d8cd4e4a19e222ee0fe4cbf09205e3
Adrian L Lange <git@p3lim.net>
2021-11-19 16:21:53 +0100

    Only require FWPP token if upgrading with the script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,17 @@
 # exit on errors
 set -e
 
-if [[ -z "$FWPP_TOKEN" ]]; then
-	echo "Missing envvar 'FWPP_TOKEN', required to authenticate with FWPP repo."
-	echo "https://downloads.linux.hpe.com/sdr/project/fwpp/"
-	exit 1
-fi
-
-# write the FWPP token to dnf vars so the repo can read it
-echo "$FWPP_TOKEN" > /etc/dnf/vars/fwpptoken
-
 # shorthand for firmware upgrades
 if [[ "${1,,}" = "upgrade" ]]; then
+	if [[ -z "$FWPP_TOKEN" ]]; then
+		echo "Missing envvar 'FWPP_TOKEN', required to authenticate with FWPP repo."
+		echo "https://downloads.linux.hpe.com/sdr/project/fwpp/"
+		exit 1
+	fi
+
+	# write the FWPP token to dnf vars so the repo can read it
+	echo "$FWPP_TOKEN" > /etc/dnf/vars/fwpptoken
+
 	bash /upgrade-firmware.sh
 fi
 
